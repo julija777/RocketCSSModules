15dc17b84b6cd3277d5935b22667bef18251188e
Julija Stepanova <s.julija@me.com>
2022-05-10 16:57:50 +0100

    components created and modules for each component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import styles from './App.module.css';
+import Title from './Title/Title';
+import Background from './Background/Backdround';
+import Grid from './Grid/Grid';
+import Rocket from './Rocket/Rocket';
+import Smoke from './Rocket/Smoke'
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Title />
+      <Background />
+      <Grid />
+      <Rocket />
+      <Smoke />
+
+    </div>
+  );
+}
+
+export default App;
